33b6146f3dfa705b533b2fc15f01654add7ed723
CharanKumarH <131192607+CharanKumarH@users.noreply.github.com>
2023-04-18 23:56:13 +0530

    Add files via upload

---
+++ Example01.java
@@ -0,0 +1,11 @@
+package ram;
+
+
+public class DEtails {
+    public static void main(String[] args) {
+        System.out.println("Java program with diff file name and class name");
+      }
+}
+
+
+
